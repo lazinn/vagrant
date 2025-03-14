8db2f137ba0e8fa41d337599fd12991649985444
Lars Zinnkann <Lars.Zinnkann@kamax.com>
2025-03-14 12:21:23 +0100

    fixed db skript

--- Dateien/web_db/createdb.sh
+++ Dateien/web_db/createdb.sh
@@ -1,9 +1,7 @@
  #!/bin/bash
-2 mysql -u root <<-EOSQL
-3 CREATE DATABASE studentendb;
-4 CREATE USER 'webuser'@'192.168.60.10' IDENTIFIED BY
-'sicheres_passwort';
-5 GRANT ALL PRIVILEGES ON studentendb.* TO
-'webuser'@'192.168.60.10';
-6 FLUSH PRIVILEGES;
-7 EOSQL
+mysql -u root <<-EOSQL
+CREATE DATABASE studentendb;
+CREATE USER 'webuser'@'192.168.60.10' IDENTIFIED BY 'sicheres_passwort';
+GRANT ALL PRIVILEGES ON studentendb.* TO 'webuser'@'192.168.60.10';
+FLUSH PRIVILEGES;
+EOSQL
